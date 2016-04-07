a40b29310b8b8e6c96ef7f8d857926818c470cf9
barry323 <barry323@hotmail.com>
2016-04-07 11:05:37 +0800

    Initial Commit

---
+++ pokedex-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-test
+//
+//  Created by Barry Chan on 7/4/2016.
+//  Copyright © 2016 equiltech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
